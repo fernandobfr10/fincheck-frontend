ec96956b3ba761bbe351a4319a078097f69ab5da
Fernando Junior <fernandobfr10@gmail.com>
2023-12-11 14:18:03 -0300

    feat: create auth layout and logo component

--- src/view/layouts/AuthLayout.tsx
+++ src/view/layouts/AuthLayout.tsx
@@ -1,10 +1,35 @@
 import { Outlet } from "react-router-dom";
 
+import { Logo } from "../components/Logo";
+
+import illustration from "../../assets/illustration.png";
+
 export function AuthLayout() {
   return (
-    <div>
-      <h1>AuthLayout</h1>
-      <Outlet />
+    <div className="w-full h-full flex">
+      <div className="w-full h-full flex flex-col justify-center items-center gap-16 lg:w-1/2">
+        <Logo className="h-6 text-gray-500" />
+
+        <div className="w-full max-w-[504px] px-8">
+          <Outlet />
+        </div>
+      </div>
+
+      <div className="w-1/2 h-full justify-center items-center p-8 relative hidden lg:flex">
+        <img
+          src={illustration}
+          className="w-full h-full object-cover max-w-[656px] max-h-[960px] select-none rounded-[32px]"
+        />
+
+        <div className="max-w-[656px] bottom-8 bg-white p-10 absolute rounded-b-[32px]">
+          <Logo className="text-teal-900 h-8" />
+
+          <p className="text-gray-700 font-medium text-xl mt-6">
+            Gerencie suas finanças pessoais de uma forma simples com o fincheck,
+            e o melhor, totalmente de graça!
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
