b4eec21bcba42eb2f7813c9f51149147f4bf23a3
Fernando Junior <fernandobfr10@gmail.com>
2023-12-11 13:03:07 -0300

    feat: add react router dom and create private and public routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import { Router } from "./Router";
+
 export function App() {
-  return <h1 className="text-3xl font-bold text-red-500">Hello world!</h1>;
+  return (
+    <>
+      <Router />
+    </>
+  );
 }
