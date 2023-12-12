f9497d7b8731a27003be12e2d8b463773161da29
Fernando Junior <fernandobfr10@gmail.com>
2023-12-12 16:01:41 -0300

    feat: create dashboard layout and accounts and transactions components

---
+++ src/view/pages/Dashboard/components/Accounts.tsx
@@ -0,0 +1,7 @@
+export function Accounts() {
+  return (
+    <div className="w-full h-full bg-teal-900 rounded-2xl px-4 py-8 md:p-10">
+      <div>Accounts</div>
+    </div>
+  );
+}
---
+++ src/view/pages/Dashboard/components/Transactions.tsx
@@ -0,0 +1,7 @@
+export function Transactions() {
+  return (
+    <div className="w-full h-full bg-gray-100 rounded-2xl p-10">
+      <div>Transactions</div>
+    </div>
+  );
+}
