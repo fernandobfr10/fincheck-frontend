95df5d9390b1e91820b1025c4aca87a0d8b44cee
Fernando Junior <fernandobfr10@gmail.com>
2023-12-12 15:59:42 -0300

    feat: create user menu component

---
+++ src/view/components/UserMenu.tsx
@@ -0,0 +1,9 @@
+export function UserMenu() {
+  return (
+    <div className="w-12 h-12 bg-teal-50 rounded-full flex items-center justify-center border border-teal-100">
+      <span className="text-sm tracking-[-0.5px] text-teal-900 font-medium">
+        FJ
+      </span>
+    </div>
+  );
+}
