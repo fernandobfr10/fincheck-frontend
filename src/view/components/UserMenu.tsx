3ecb5e6102ee9239e56e4a1dc50ea19f3a06e574
Fernando Junior <fernandobfr10@gmail.com>
2023-12-23 16:18:09 -0300

    feat: create dropdown manu component and implements on userMenu component

--- src/view/components/UserMenu.tsx
+++ src/view/components/UserMenu.tsx
@@ -1,9 +1,30 @@
+import { ExitIcon } from "@radix-ui/react-icons";
+
+import { DropdownMenu } from "./DropdownMenu";
+import { useAuth } from "../../app/hooks/useAuth";
+
 export function UserMenu() {
+  const { signout } = useAuth();
+
   return (
-    <div className="w-12 h-12 bg-teal-50 rounded-full flex items-center justify-center border border-teal-100">
-      <span className="text-sm tracking-[-0.5px] text-teal-900 font-medium">
-        FJ
-      </span>
-    </div>
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        <div className="w-12 h-12 bg-teal-50 rounded-full flex items-center justify-center border border-teal-100 select-none">
+          <span className="text-sm tracking-[-0.5px] text-teal-900 font-medium">
+            FJ
+          </span>
+        </div>
+      </DropdownMenu.Trigger>
+
+      <DropdownMenu.Content className="w-32">
+        <DropdownMenu.Item
+          className="flex items-center justify-between"
+          onSelect={signout}
+        >
+          Sair
+          <ExitIcon className="w-4 h-4" />
+        </DropdownMenu.Item>
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
   );
 }
